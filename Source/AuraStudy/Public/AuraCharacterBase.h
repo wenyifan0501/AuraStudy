a7cace0a3fb45c3f811c5775649778df3ad9cedc
wenyifan0501 <364650947@qq.com>
2025-10-27 18:02:04 +0800

    Lesson 3 Add
    修改basecharacter

--- Source/AuraStudy/Public/AuraCharacterBase.h
+++ Source/AuraStudy/Public/AuraCharacterBase.h
@@ -12,18 +12,8 @@ class AURASTUDY_API AAuraCharacterBase : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AAuraCharacterBase();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
