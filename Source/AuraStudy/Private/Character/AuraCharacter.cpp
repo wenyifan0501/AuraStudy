94997fb3983791b7b5f6156f5283b4e90c20d666
wenyifan0501 <364650947@qq.com>
2025-10-30 08:02:13 +0800

    Lesson 11 Add
    新增高亮接口

--- Source/AuraStudy/Private/Character/AuraCharacter.cpp
+++ Source/AuraStudy/Private/Character/AuraCharacter.cpp
@@ -10,5 +10,9 @@ AAuraCharacter::AAuraCharacter()
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
-	
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
 }
--- Source/AuraStudy/Public/Character/AuraEnemy.h
+++ Source/AuraStudy/Public/Character/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacter.h"
+#include "Interface/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURASTUDY_API AAuraEnemy : public AAuraCharacterBase
+class AURASTUDY_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
